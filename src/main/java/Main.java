230ebbf8cc7ad2a89873860f728f42f09b99ba84
kimtool <kimtool@hotmail.com>
2019-10-09 20:10:32 +0200

    hi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,5 +21,6 @@ public class Main {
             System.out.println("Buzz");            
         else 
             System.out.println(number);
+        System.out.println("testing voor git again");
     }
 }
